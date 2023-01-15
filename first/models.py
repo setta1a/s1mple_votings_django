fb88e9e6e19f3cf8b9ec0953b85818227419949d
Simon Anikin <s.anikin1309@gmail.com>
2023-01-15 15:51:42 +0300

    Changing db and implementing it

--- first/models.py
+++ first/models.py
@@ -1,10 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
 
-class DiscreteVotings(models.Model):
-    theme = models.CharField(max_length=100)
-    first_option = models.CharField(max_length=50)
-    second_option = models.CharField(max_length=50)
-    first_option_count = models.IntegerField()
-    second_option_count = models.IntegerField()
+class Voting(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=175)
+    voting_type = models.IntegerField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, default=1)
+
+
+class VoteVariant(models.Model):
+    description = models.CharField(max_length=50)
+    voting = models.ForeignKey(to=Voting, on_delete=models.CASCADE, default=1)
+
+    def calculateVotes(self):
+        return VoteFact.objects.filter(variant=self).count()
+
+    votes_count = property(calculateVotes)
+
+
+class VoteFact(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, default=1)
+    variant = models.ForeignKey(to=VoteVariant, on_delete=models.CASCADE, default=1)
+    created_at = models.DateTimeField()
--- first/views.py
+++ first/views.py
@@ -1,23 +1,21 @@
-from django.http import HttpResponseRedirect
+import datetime
+
 from django.shortcuts import render, get_object_or_404
 
-from first.models import DiscreteVotings
+from first.models import Voting, VoteVariant, VoteFact
 
 
 def voting_page(request, voting_id):
     context = {}
     if request.method == "POST":
-        print(request.POST)
-        if request.POST['variant']:
-            t = DiscreteVotings.objects.get(id=voting_id)
-            if request.POST['variant'] == t.first_option:
-                t.first_option_count += 1
-            elif request.POST['variant'] == t.second_option:
-                t.second_option_count += 1
-            t.save()
-            # return HttpResponseRedirect('/voting/')
+        if request.POST.get('variant_id') and request.user.is_authenticated and not VoteFact.objects.filter(author = request.user):
+            record = VoteFact(variant_id=request.POST['variant_id'],
+                              created_at = datetime.datetime.now(),
+                              author=request.user)
+            record.save()
 
-    context['VOting'] = get_object_or_404(DiscreteVotings, id=voting_id)
+    context['voting'] = get_object_or_404(Voting, id=voting_id)
+    context["voting_variants"] = VoteVariant.objects.filter(voting = voting_id)
     return render(request, 'voting.html', context)
 
 
