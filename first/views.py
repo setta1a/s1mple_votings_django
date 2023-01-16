31127b4c0dcf7da3615457f9b549c49859048d7f
Andrey Sitalo <andreysitalo09@gmail.com>
2023-01-16 20:38:01 +0300

    save smth

--- first/views.py
+++ first/views.py
@@ -7,22 +7,30 @@ from first.models import Voting, VoteVariant, VoteFact
 
 def voting_page(request, voting_id):
     context = {}
+    context['voting'] = get_object_or_404(Voting, id=voting_id)
+    context["voting_variants"] = VoteVariant.objects.filter(voting=voting_id)
     if request.method == "POST":
-        if request.POST.get('variant_id') and request.user.is_authenticated and not VoteFact.objects.filter(author = request.user):
+        is_voted = False
+        for votefact in VoteFact.objects.filter(author = request.user):
+            if VoteVariant.objects.filter(id=votefact.variant_id).filter(voting_id=voting_id).count() != 0:
+                is_voted = True
+                context["is_voted"] = is_voted
+                break
+
+        if request.user.is_authenticated and not is_voted:
             record = VoteFact(variant_id=request.POST['variant_id'],
                               created_at = datetime.datetime.now(),
                               author=request.user)
             record.save()
 
-    context['voting'] = get_object_or_404(Voting, id=voting_id)
-    context["voting_variants"] = VoteVariant.objects.filter(voting = voting_id)
+
     return render(request, 'voting.html', context)
 
 
 def list_of_votings_page(request):
     context = {}
+    context["votings_list"] = Voting.objects.all()
     return render(request, 'list_of_votings.html', context)
-
 
 def authorization_page(request):
     context = {}
