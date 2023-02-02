ecefd217f09e986f86be7ad8aaa8797e15ef5a3e
leonid perkin <perkin.leonid@ya.ru>
2023-02-03 00:28:31 +0300

    add_init_documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'S1mple_votings'
+copyright = '2023, Аникин Семен,Ситало Андрей,Весенев Валерий,Перкин Леонид,Быков Владимир'
+author = 'Аникин Семен,Ситало Андрей,Весенев Валерий,Перкин Леонид,Быков Владимир'
+release = '0.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
